7d4910af8929c142f6fbdb929e2a2315da4033d1
Brenton Mumford <Brenton.Mumford@ipreo.com>
2018-03-28 10:00:30 -0400

    video 01 complete.

---
+++ src/products/store/actions/pizzas.action.ts
@@ -0,0 +1,26 @@
+import { Action } from '@ngrx/store';
+import { Pizza } from '../../models/pizza.model';
+
+// load pizzas here
+export const LOAD_PIZZAS = '[Products] Load Pizzas';
+export const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail';
+export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
+
+// the implement thing is kind of just a typechecking process
+// an action is a specific type we're grabbing from the store
+export class LoadPizzas implements Action {
+    readonly type = LOAD_PIZZAS;
+}
+
+export class LoadPizzasFail implements Action {
+    readonly type = LOAD_PIZZAS_FAIL;
+    constructor(public payload: any) {}
+}
+
+export class LoadPizzasSuccess implements Action {
+    readonly type = LOAD_PIZZAS_SUCCESS;
+    constructor(public payload: Pizza) {}
+}
+
+// export the action types for use in reducer
+export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
